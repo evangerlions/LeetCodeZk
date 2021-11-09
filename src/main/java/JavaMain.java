b5dcb63f4a1f18d22c4cc41425ab8eb07e5ea9b6
evangerlions <evangerlions@gmail.com>
2021-11-10 00:06:33 +0800

    ADD: offer44

--- src/main/java/JavaMain.java
+++ src/main/java/JavaMain.java
@@ -5,21 +5,8 @@ import java.util.stream.Collectors;
 public class JavaMain {
 
     public static void main(String[] args) {
-        System.out.println(new MyTestString());
-        ArrayList<Integer> linkedList = new ArrayList<Integer>(); {}
-        Random r = new Random();
-        int [] aaa = new int[2];
-        for (int i = 0; i < 10; i++) {
-            linkedList.add(r.nextInt(100));
-        }
-         HashMap<Character, Boolean> hm = new HashMap<>();
-       int a = (int)(1L);
-        System.out.println(linkedList);
-        System.out.println();
-        System.out.println(linkedList.stream().map(i -> i * i).filter(i -> i / 2 != 0).sorted().collect(Collectors.toUnmodifiableList()));
-
-        System.out.println(linkedList);
-
+        String str = "12345";
+        System.out.println(str.charAt(1));
     }
 
     private void print() {
