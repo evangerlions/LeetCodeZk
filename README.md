628d9dec9d93e9ee9eb6995b34a49a142ab91f11
evangerlions <evangerlions@gmail.com>
2021-11-13 13:14:14 +0800

    ADD: offer45

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ offer16 快速幂
 
 offer44 边界条件很多
 
+[剑指 Offer 45. 把数组排成最小的数](https://leetcode-cn.com/problems/ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof/)
+
+巧解，自定义排序
+
 offer50  
 基础：string 的遍历，map 的遍历
 
---
+++ src/main/java/leetcode/offer45_minNum/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.offer45_minNum;
+
+import java.util.LinkedList;
+
+/**
+ * https://leetcode-cn.com/problems/ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof/
+ * 剑指 Offer 45. 把数组排成最小的数
+ */
+public class Solution {
+    public String minNumber(int[] nums) {
+        if (nums.length == 0) return "";
+
+        LinkedList<String> numStrs = new LinkedList<>();
+
+        for (int num : nums) {
+            String str = Integer.toString(num);
+            numStrs.add(str);
+        }
+
+        numStrs.sort((str1, str2) -> (str1 + str2).compareTo(str2 + str1));
+
+        return String.join("", numStrs);
+    }
+}
